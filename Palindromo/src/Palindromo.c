21df13cf019b5337c1e5472883f162e1b639a56d
HelenoJuniorFernandes <165923583+HelenoJuniorFernandes@users.noreply.github.com>
2024-04-03 11:37:00 -0300

    Add files via upload

---
+++ Palindromo/src/Palindromo.c
@@ -0,0 +1,42 @@
+/*
+ ============================================================================
+ Name        : Palindromo.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+	char palavra [100];
+	int i, j, tamanho;
+	int palindromo = 1;
+
+	printf("Digite uma palavra: ");
+	scanf("%s", palavra);
+
+	tamanho = strlen(palavra);
+
+	//Verificar se é um palindromo
+	for(i = 0, j = tamanho - 1; i < j; i++, j--)  {
+
+		printf("i: %c\n", palavra[i]);
+		printf("j: %c\n", palavra[j]);
+		if(palavra [i] != palavra[j]) {
+		 palindromo = 0
+				 break;
+
+		}
+	}
+if(palindromo){
+	printf("\nA palavra é um palindromo.\n");
+}	else{
+	printf("\nA palavra nao é um palindromo.\n");
+}
+}
+
